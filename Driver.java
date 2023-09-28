dd51199a9be164d351492e5dd63c2da17be788a7
Yiran Gogo Yu <gogoyiranyu@gmail.com>
2023-09-28 16:03:51 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,18 +1,29 @@
 package b07lab1;
 
+import java.io.*;
+
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6, 0, 0, 5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0, -2, 0, 0, -9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+
+        double[] c1 = {6.0, 0.0, 0.0, 5.0};
+        int[] e1 = {0, 1, 2, 3};
+        double[] c2 = {0.0, -2.0, 0.0, 0.0, -9.0};
+        int[] e2 = {0, 1, 2, 3, 4, 5};
+        Polynomial p1 = new Polynomial(c1, e1);
+        Polynomial p2 = new Polynomial(c2, e2);
+        Polynomial sum = p1.add(p2);
+        System.out.println("s(0.1) = " + sum.evaluate(0.1));
+
+        Polynomial product = p1.multiply(p2);
+        System.out.println("product(0.1) = " + product.evaluate(0.1));
+
+        if(sum.hasRoot(1)) System.out.println("1 is a root of s");
+        else System.out.println("1 is not a root of s");
+
+        File file = new File("/Users/gogo/b07lab1/text.txt");
+        Polynomial new_polynomial = new Polynomial(file);
+        new_polynomial.saveToFile("/Users/gogo/b07lab1/text.txt");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,18 +1,87 @@
 package b07lab1;
 
+import java.io.*;
+
 public class Polynomial {
     double[] coefficients = new double[100];
-    int index; // counter variable
+    int[] exponents = new int[100];
+    int index;
     public Polynomial() {
         for (index = 0; index < coefficients.length; index++) {
             coefficients[index] = 0.0;
+            exponents[index] = index;
         }
     }
 
-    public Polynomial(double[] c) {
+    public Polynomial(double[] c, int[] e) {
         for (index = 0; index < c.length; index++) {
             coefficients[index] = c[index];
+            exponents[index] = e[index];
         }
+    }
+
+    public Polynomial(File f) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(f));
+        String line = reader.readLine();
+        String[] parts = line.split("(?=-)|(\\+)");
+        for (String sub : parts) {
+            if (sub.contains("x")) {
+                if (sub.contains("-")) {
+                    exponents[Integer.parseInt(sub.substring(3, 4))] = Integer.parseInt(sub.substring(3, 4));
+                    coefficients[Integer.parseInt(sub.substring(3, 4))] = Double.parseDouble(sub.substring(0, 2));
+                } else {
+                    exponents[Integer.parseInt(sub.substring(2, 3))] = Integer.parseInt(sub.substring(2, 3));
+                    coefficients[Integer.parseInt(sub.substring(2, 3))] = Double.parseDouble(sub.substring(0, 1));
+                }
+            } else {
+                exponents[0] = 0;
+                coefficients[0] = Double.parseDouble(sub);
+            }
+        }
+        reader.close();
+    }
+    public Polynomial multiply(Polynomial p) {
+        int i, j, k;
+        Polynomial product = new Polynomial();
+        for (i = 0; i < this.coefficients.length; i++) {
+            for (j = 0; j < p.coefficients.length; j++) {
+                k = this.exponents[i] + p.exponents[j];
+                product.coefficients[k] += this.coefficients[i] * p.coefficients[j];
+            }
+        }
+        for (int idx = 0; idx < product.coefficients.length; idx++) {
+            if (product.coefficients[idx] == 0.0) {
+                product.exponents[idx] = 0;
+            }
+        }
+        return product;
+    }
+
+    public void saveToFile(String s) throws Exception {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(s));
+        int idx = 1;
+        boolean flag = true;
+        if (this.coefficients[0] != 0.0) writer.write(String.valueOf((int)this.coefficients[0]));
+        while ((idx < this.coefficients.length)) {
+            if (this.coefficients[idx] == 0.0) {
+                idx++;
+                continue;
+            }
+            else if (this.coefficients[idx] < 0) {writer.write(String.valueOf((int)this.coefficients[idx]));}
+            else {
+                for (int i = 0; i < idx; i++) {
+                    if (this.coefficients[i] != 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {writer.write(String.valueOf((int)this.coefficients[idx]));}
+                else {writer.write("+" + (int)this.coefficients[idx]);}
+            }
+            writer.write("x" + this.exponents[idx]);
+            idx++;
+        }
+        writer.close();
     }
 
     public Polynomial add(Polynomial p) {
